3e1a5d01778592a1717c703f06cd3f526fc240b2
Arnaud Lemaire <alemaire@norad.fr>
2014-09-28 16:53:25 +0200

    first version with archlinux base and nginx

---
+++ update-all-images.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+# simple script to update all images
+
+echo "Required root password to bootstrap archlinux base image :"
+sudo arch/build-and-import-base-image-archlinux.sh
+
+IMGS="
+arch-nginx
+arch-nginx-php
+"
+
+for img in $IMGS ; do
+  cd $img
+  echo "updating $img"
+  echo -e "\e[101mupdating $img :\e[0;m"
+  docker build -t n0rad/$img .
+  cd -
+done
