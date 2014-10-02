ccde7e29b8a0482fc887df8fa02b3bd9227b9721
Arnaud Lemaire <alemaire@norad.fr>
2014-10-02 13:52:29 +0200

    skip base image on rebuild all

--- update-all-images.sh
+++ update-all-images.sh
@@ -4,12 +4,14 @@
 trap 'exit' ERR
 
 echo "Required root password to bootstrap archlinux base image :"
-#sudo arch/build-and-import-base-image-archlinux.sh
+sudo arch/build-and-import-base-image-archlinux.sh
 
 for i in $(ls -d */ | sort -r); do
-  cd "$i"
-  echo "updating ${i%%/}"
-  echo -e "\e[101mupdating ${i%%/} :\e[0;m"
-#  docker build -t "n0rad/${i%%/}" .
-  cd -
+  if [ "$i" != "arch/" ]; then
+    cd "$i"
+    echo "updating ${i%%/}"
+    echo -e "\e[101mupdating ${i%%/} :\e[0;m"
+    docker build -t "n0rad/${i%%/}" .
+    cd -
+  fi
 done
