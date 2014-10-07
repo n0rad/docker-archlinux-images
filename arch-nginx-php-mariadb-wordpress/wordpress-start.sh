7e6685b9d229c3947833ef77dd37ceb6cf86df7d
Arnaud Lemaire <alemaire@norad.fr>
2014-10-07 21:21:13 +0200

    Working wordpress with mariadb and auto bootstrap

--- arch-nginx-php-mariadb-wordpress/wordpress-start.sh
+++ arch-nginx-php-mariadb-wordpress/wordpress-start.sh
@@ -1,8 +1,18 @@
-#!/bin/bash
+#!/bin/bash -x
+
+
 if [ ! -f /srv/http/wp-config.php ]; then
 
-  sed -e "s/database_name_here/$WORDPRESS_DB/
-  s/username_here/$WORDPRESS_DB/
+  WORDPRESS_PASSWORD=$(pwgen -c -n -1 65)
+  MYSQL_PASSWORD=`pwgen -c -n -1 12`
+  echo $MYSQL_PASSWORD > /mysql-root-pw.txt 
+  echo "WAITING 30s FOR MYSQL TO START"
+  sleep 30
+  /usr/bin/mysqladmin -u root password "$MYSQL_PASSWORD"
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
+
+  sed -e "s/database_name_here/wordpress/
+  s/username_here/wordpress/
   s/password_here/$WORDPRESS_PASSWORD/
   /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
@@ -13,9 +23,8 @@ if [ ! -f /srv/http/wp-config.php ]; then
   /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /srv/http/wp-config-sample.php > /srv/http/wp-config.php
 
-
-
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
-
-
 fi
+
+
+
+
--- arch-nginx-php-mariadb/mariadb-start.sh
+++ arch-nginx-php-mariadb/mariadb-start.sh
@@ -1,14 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
 
 shopt -s nullglob
 files=(/var/lib/mysql/*)
 if [ ${#files[@]} -eq 0 ]; then
-  MYSQL_PASSWORD=`pwgen -c -n -1 12`
-  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
   /usr/bin/mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-  /usr/bin/mysqladmin -u root password "$MYSQL_PASSWORD"
 fi
-
 
 su - mysql -s /bin/sh -c '/usr/bin/mysqld --pid-file=/run/mysqld/mysqld.pid --skip-name-resolve --datadir=/var/lib/mysql'
 
