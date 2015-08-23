e0728c74d8fa9068482be6b82b21ec377c99e911
n0rad <alemaire@norad.fr>
2015-08-23 21:52:55 +0000

    include nginx to have only 1 input -> 80

--- arch-mariadb-seafile/seafile-start.sh
+++ arch-mariadb-seafile/seafile-start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 sleep 10
 
-/opt/seafile-server-latest/seafile.sh start && /opt/seafile-server-latest/seahub.sh start
+/opt/seafile-server-latest/seafile.sh start
+sleep 10
+/opt/seafile-server-latest/seahub.sh start-fastcgi
 
 while true; do sleep 10; done
 
